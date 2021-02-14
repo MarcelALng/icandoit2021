5fdf451c21b0caeeed197c8a006a328ba3069a72
Marcel AL NG <43136183+MarcelALng@users.noreply.github.com>
2021-02-14 22:35:25 +0100

    add floating button & background with appbar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -9,7 +9,15 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('ICanDOIt'),
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+      ),
+      backgroundColor: Color(0xff414a4c),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.orange[700],
         onPressed: () {},
         child: Icon(Icons.add),
       ),
