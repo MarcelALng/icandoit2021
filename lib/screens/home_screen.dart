936f888e1b544c076c0e7f4bd9cf51799aeb0e66
Marcel AL NG <43136183+MarcelALng@users.noreply.github.com>
2021-02-15 01:09:44 +0100

    add  bottomsheet

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,9 +6,13 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  final GlobalKey<ScaffoldState> scaffoldKey =
+      GlobalKey(); // key which can be use in all application
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: scaffoldKey,
       appBar: AppBar(
         title: Text('ICanDOIt'),
         backgroundColor: Colors.transparent,
@@ -18,9 +22,21 @@ class _HomeState extends State<Home> {
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.orange[700],
-        onPressed: () {},
+        onPressed: () {
+          buildBottomSheet();
+        },
         child: Icon(Icons.add),
       ),
     );
+  }
+
+  PersistentBottomSheetController buildBottomSheet() {
+    return scaffoldKey.currentState.showBottomSheet((builder) {
+      return Container(
+        alignment: Alignment.center,
+        height: 300,
+        child: Text("Hello 안녕"),
+      );
+    });
   }
 }
