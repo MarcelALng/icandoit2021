41a76bd0cd2669e54f546bcb0b4ff740589c743c
MarcelALng <marcel.nguyen@gmail.com>
2021-02-21 05:50:42 +0100

    [Home/Screen Alert Dialong OK

--- lib/screens/components/build_challenges_list.dart
+++ lib/screens/components/build_challenges_list.dart
@@ -18,67 +18,71 @@ class _ChallengeListBuilderState extends State<ChallengeListBuilder> {
     return ListView.builder(
       itemCount: _challengesList.length,
       itemBuilder: (context, index) {
-        return Dismissible(
-          confirmDismiss: (direction) {
-            if (direction == DismissDirection.startToEnd) {
-              showDialog(context:context,
-                builder :(context), 
-              { return AlertDialog(
-                title: Text("test title"),
-                content:
-                    Text("Etes-vous sur de vouloir supprimer le challenge?"),
-                actions: <Widget>[
-                  RaisedButton(
-                    onPressed: () {},
-                    child: Text("oui"),
+        return Padding(
+          padding: const EdgeInsets.only(bottom: 3.0, left: 8.0, right: 8.0),
+          child: Dismissible(
+            confirmDismiss: (direction) {
+              if (direction == DismissDirection.startToEnd) {
+                showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        title: Text("Suppression"),
+                        content: Text(
+                            "Etes-vous sur de vouloir supprimer le challenge?"),
+                        actions: <Widget>[
+                          RaisedButton(
+                            onPressed: () {},
+                            child: Text("oui"),
+                          ),
+                          RaisedButton(
+                            onPressed: () {},
+                            child: Text("Non"),
+                          )
+                        ],
+                      );
+                    });
+              }
+            },
+            background: Container(
+              color: Colors.red,
+              padding: EdgeInsets.only(bottom: 10.0, left: 8.0, right: 8.0),
+              alignment: Alignment.centerLeft,
+              child: Icon(Icons.delete),
+            ),
+            secondaryBackground: Container(
+              padding: EdgeInsets.only(right: 20.0),
+              alignment: Alignment.centerRight,
+              color: Colors.green,
+              child: Icon(Icons.check),
+            ),
+            key: Key(UniqueKey().toString()),
+            child: Container(
+              color: Colors.white,
+              child: ListTile(
+                title: Text(_challengesList[index].name),
+                subtitle: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: <Widget>[
+                      Text(
+                        "Objectif:",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, color: Colors.black),
+                      ),
+                      SizedBox(width: 10.0),
+                      Text(_challengesList[index].target.toString()),
+                      SizedBox(width: 5.0),
+                      Text(_challengesList[index]
+                          .unity
+                          .toString()
+                          .replaceAll(unityPattern, "")
+                          .toUpperCase()),
+                    ],
                   ),
-                  RaisedButton(
-                    onPressed: () {},
-                    child: Text("Non"),
-                  )
-                ],
-              );
-            });
-            }
-          },
-          background: Container(
-            color: Colors.red,
-            padding: EdgeInsets.only(bottom: 10.0, left: 8.0, right: 8.0),
-            alignment: Alignment.centerLeft,
-            child: Icon(Icons.delete),
-          ),
-          secondaryBackground: Container(
-            padding: EdgeInsets.only(right: 20.0),
-            alignment: Alignment.centerRight,
-            color: Colors.green,
-            child: Icon(Icons.check),
-          ),
-          key: Key(UniqueKey().toString()),
-          child: Container(
-            color: Colors.white,
-            child: ListTile(
-              title: Text(_challengesList[index].name),
-              subtitle: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  children: <Widget>[
-                    Text(
-                      "Objectif:",
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold, color: Colors.black),
-                    ),
-                    SizedBox(width: 10.0),
-                    Text(_challengesList[index].target.toString()),
-                    SizedBox(width: 5.0),
-                    Text(_challengesList[index]
-                        .unity
-                        .toString()
-                        .replaceAll(unityPattern, "")
-                        .toUpperCase()),
-                  ],
                 ),
+                isThreeLine: true,
               ),
-              isThreeLine: true,
             ),
           ),
         );
